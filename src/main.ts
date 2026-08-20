6b450a1f51d820cd8f5113bc84e833d19a307c5a
xzq <zhiqiang.xiong@ysa365.com>
2026-08-20 17:22:35 +0800

    feat(main): 添加 ElementPlus 组件库的中文语言支持

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import ElementPlus from "element-plus";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
 import "element-plus/dist/index.css";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
@@ -9,7 +10,7 @@ import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
-app.use(ElementPlus);
+app.use(ElementPlus, { locale: zhCn });
 app.use(router);
 app.mount("#app");
 
